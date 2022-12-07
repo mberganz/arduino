c2a34462382724a57529c6798feff6e02debe314
mberganz <108034006+mberganz@users.noreply.github.com>
2022-12-07 14:12:58 -0300

    millis

--- arduino.ino
+++ arduino.ino
@@ -14,13 +14,15 @@ char sentenca_report[128];
 char sentenca_open[128];
 char leitura[8];
 char distancia_char[8];
+const int intervalo = 1000;
+unsigned long tempo_anterior = 0;
 
 const int tipo_vaga = 1;
 const int num_vaga = 1;
 
 byte mac_addr[] = { 0xF4, 0x6B, 0x8C, 0x68, 0xB5, 0xF9 };
 
-IPAddress server_addr(10,10,204,24);
+IPAddress server_addr(10, 10, 204, 24);
 char user[] = "arduino";
 char password[] = "arduino";
 
@@ -35,15 +37,16 @@ MySQL_Connection conn((Client *)&client);
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial)
+    ;
   Ethernet.begin(mac_addr);
   Serial.println("Conectando...");
   if (conn.connect(server_addr, 3306, user, password)) {
     Serial.println("Conectado!");
-    
+
     delay(1000);
-    
-    MySQL_Cursor *cur_mem = new MySQL_Cursor (&conn);
+
+    MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
     cur_mem->execute(BANCODEDADOS);
     delete cur_mem;
   } else {
@@ -64,11 +67,9 @@ void loop() {
   Serial.println("Lendo dados do sensor...");
 
   // ------------------------------------------------ //
-  
+
   float cmMsec;
   int distancia;
-  String distancia_str = String(distancia_char);
-  unsigned long tempo;
   long microsec = ultrasonic.timing();
   cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
 
@@ -79,26 +80,39 @@ void loop() {
   Serial.print(distancia_char);
   Serial.println("cm");
 
+  String distancia_str = String(distancia_char);
+  distancia = distancia_str.toInt();
+  Serial.println(distancia);
+
   delay(1000);
 
   // ------------------------------------------------ //
 
-  distancia = distancia_str.toInt();
-  Serial.println(distancia);
-  if (distancia < 10) {
-    tempo = millis();
-    String tempo_str = String(tempo, DEC);  
-    Serial.print("Tempo: ");
-    Serial.println(tempo_str);
+  if (distancia <= 11 && distancia > 10) {
+    Serial.println("Entrando");
+  } else if (distancia > 10 && distancia < 11) {
+    Serial.println("Saindo");
+  } else if (distancia <= 10) {
+    Serial.println("Dentro");
+    unsigned long tempo_atual = millis();
+    if ((unsigned long)(tempo_atual - tempo_anterior) >= intervalo) {
+      String tempo_str = String(tempo_atual, DEC);
+      Serial.print("Tempo: ");
+      Serial.println(tempo_str);
 
-    // ------------------------------------------------ //
+      // ------------------------------------------------ //
 
-    Serial.println("Executando sentença tempo");
-    sprintf(sentenca_tempo, INSERIR_TEMPO, tempo_str);
-    Serial.println(sentenca_tempo);
-    cur_mem->execute(sentenca_tempo);
+      Serial.println("Executando sentença tempo");
+      sprintf(sentenca_tempo, INSERIR_TEMPO, tempo_str);
+      Serial.println(sentenca_tempo);
+      cur_mem->execute(sentenca_tempo);
+
+      // ------------------------------------------------ //
+
+      tempo_anterior = tempo_atual;
+    }
   }
-  
+
   delay(1000);
 
   // ------------------------------------------------ //
@@ -114,6 +128,7 @@ void loop() {
 
   Serial.println("Liberando memória...");
   delete cur_mem;
+  delay(1000);
   Serial.println("Memória liberada");
 
   delay(1000);
