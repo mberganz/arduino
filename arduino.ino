a6a53e6ec456bf40ed8e5de74d10f0ae54db7a4a
Mikael Berganzini <108034006+mberganz@users.noreply.github.com>
2022-12-09 21:45:37 -0300

    Add files via upload

--- arduino.ino
+++ arduino.ino
@@ -1,3 +1,4 @@
+#include <Servo.h>
 #include <Ultrasonic.h>
 #include <MySQL_Connection.h>
 #include <MySQL_Cursor.h>
@@ -6,6 +7,8 @@
 
 #define pino_trigger 4
 #define pino_echo 5
+#define pino_pir 2
+#define SERVO 6
 
 Ultrasonic ultrasonic(pino_trigger, pino_echo);
 
@@ -18,22 +21,26 @@ char leitura[8];
 char distancia_char[8];
 const int intervalo = 10000;
 unsigned long tempo_anterior = 0;
+unsigned long tempo_atual;
 bool open = false;
+int acionamento;
+Servo s;
+int posicao;
 
 const int tipo_vaga = 1;
 const int num_vaga = 1;
 
-byte mac_addr[] = { 0xF4, 0x6B, 0x8C, 0x68, 0xB5, 0xF9 };
+byte mac_addr[] = { 0x00, 0x27, 0x13, 0xAE, 0x7E, 0xD1 };
 
-IPAddress server_addr(10, 10, 204, 24);
+IPAddress server_addr(10, 10, 117, 18);
 char user[] = "arduino";
 char password[] = "arduino";
 
-char INSERIR_TEMPO[] = "INSERT INTO report (duration) VALUES (%s)";
-char INSERIR_REPORT[] = "INSERT INTO report (open) VALUES 1";
-char UPDATE_REPORT[] = "UPDATE report SET open = 0";
-char UPDATE_OPEN[] = "UPDATE park SET open = 1 WHERE park_id = 'num_vaga'";
-char UPDATE_CLOSE[] = "UPDATE park SET open = 0 WHERE park_id != 'num_vaga'";
+char UPDATE_TEMPO[] = "UPDATE report SET duration = (%d)";
+char UPDATE_REPORT_OPEN[] = "UPDATE report SET open = 1";
+char UPDATE_REPORT_CLOSE[] = "UPDATE report SET open = 0";
+char UPDATE_OPEN[] = "UPDATE park SET open = 1";
+char UPDATE_CLOSE[] = "UPDATE park SET open = 0";
 char BANCODEDADOS[] = "USE arduino";
 
 EthernetClient client;
@@ -57,6 +64,10 @@ void setup() {
     Serial.println("A conexão falhou");
     conn.close();
   }
+
+  pinMode(pino_pir, INPUT);
+  s.attach(SERVO);
+  s.write(0);
 }
 
 void loop() {
@@ -94,9 +105,11 @@ void loop() {
 
   if (distancia > 11) {
     Serial.println("Vaga livre");
-    sprintf(sentenca_close, UPDATE_CLOSE);  // Marca vaga como livre
+    sprintf(sentenca_close, UPDATE_OPEN);  // Marca vaga como livre
     Serial.println(sentenca_close);
     cur_mem->execute(sentenca_close);
+    
+    tempo_anterior = tempo_atual;
 
     delay(1000);
   } else if (distancia <= 11 && distancia > 10) {
@@ -106,32 +119,46 @@ void loop() {
     Serial.println("Dentro");
 
     Serial.println("Atualizando vaga no banco");
-    sprintf(sentenca_open, UPDATE_OPEN);  // Marca vaga como ocupada
+    sprintf(sentenca_open, UPDATE_CLOSE);  // Marca vaga como ocupada
     Serial.println(sentenca_open);
     cur_mem->execute(sentenca_open);
 
     delay(1000);
 
-    unsigned long tempo_atual = millis();
+    tempo_atual = millis();
     if ((unsigned long)(tempo_atual - tempo_anterior) >= intervalo) {
-      String tempo_str = String(tempo_atual, DEC);
+      String tempo_str = String(tempo_atual);
       Serial.print("Tempo: ");
       Serial.println(tempo_str);
+
+      delay(1000);
 
       // ---------- Salva o tempo de vaga no banco ---------- //
 
       Serial.println("Executando sentença tempo");
-      sprintf(sentenca_tempo, INSERIR_TEMPO, tempo_str);
+      sprintf(sentenca_tempo, UPDATE_TEMPO, tempo_atual - tempo_anterior);
       Serial.println(sentenca_tempo);
       cur_mem->execute(sentenca_tempo);
 
       // ----------------- Reinicia o timer ----------------- //
 
-      tempo_anterior = tempo_atual;
+      // tempo_anterior = tempo_atual;
     }
   }
 
   delay(1000);
+
+  // ----------------- Sensor de presença ----------------- //
+
+  acionamento = digitalRead(pino_pir);
+
+  if (acionamento == LOW) {
+    posicao = 0;
+    s.write(posicao);
+  } else {
+    posicao = 90;
+    s.write(posicao);
+  }
 
   // --------------- Reportar para o sistema --------------- //
 
@@ -143,12 +170,12 @@ void loop() {
 
   if (open == true) {
     Serial.println("Executando sentença report");
-    sprintf(sentenca_report, INSERIR_REPORT, "report");  // Marca vaga como ocupada
+    sprintf(sentenca_report, UPDATE_REPORT_CLOSE);  // Marca vaga como ocupada
     Serial.println(sentenca_report);
     cur_mem->execute(sentenca_report);
   } else if (open == false) {
     Serial.println("Atualizando report");
-    sprintf(sentenca_update_report, UPDATE_REPORT);  // Marca vaga como livre
+    sprintf(sentenca_update_report, UPDATE_REPORT_OPEN);  // Marca vaga como livre
     Serial.println(sentenca_update_report);
     cur_mem->execute(sentenca_update_report);
   }
